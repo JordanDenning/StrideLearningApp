bc5dd580883a51bb29f6cd08dd63285f284f0240
Jordan Denning <jode8036@colorado.edu>
2020-01-18 11:58:51 -0700

    Initial Commit

---
+++ StrideLearningApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrideLearningApp
+//
+//  Created by Jordan Denning on 1/18/20.
+//  Copyright © 2020 Jordan Denning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
