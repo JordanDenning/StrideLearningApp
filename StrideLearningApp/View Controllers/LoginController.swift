364166173988622f6f955647b0d73978258b0a9d
Brittany Choy <brch1381@colorado.edu>
2020-01-29 09:05:33 -0700

    Resend email confirmation bug fix

--- StrideLearningApp/View Controllers/LoginController.swift
+++ StrideLearningApp/View Controllers/LoginController.swift
@@ -128,9 +128,7 @@ class LoginController: UIViewController {
                 if !user.isEmailVerified {
                     let alertVC = UIAlertController(title: "Verify Email", message: "You must verify your email before logging in. Would you like us to resend another email confirmation link?", preferredStyle: UIAlertController.Style.alert)
 //                    let okAction = UIAlertAction(title: "Okay", style: .default)
-                    let resendAction = UIAlertAction(title: "Resend", style: UIAlertAction.Style.default) {(_) in Auth.auth().currentUser?.sendEmailVerification { (error) in
-                        // ...
-                        }
+                    let resendAction = UIAlertAction(title: "Resend", style: UIAlertAction.Style.default) {(_) in user.sendEmailVerification()
                     }
                     let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil)
                     
