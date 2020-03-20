e4edfa1d21b40fbe74ca0fbef79f6293f9be24a8
Jordan Denning <jode8036@colorado.edu>
2020-03-20 15:53:43 -0600

    screen size extension

--- StrideLearningApp/Helpers/ScreenSizeExtension.swift
+++ StrideLearningApp/Helpers/ScreenSizeExtension.swift
@@ -2,8 +2,27 @@
 //  ScreenSizeExtension.swift
 //  StrideLearningApp
 //
-//  Created by Jordan Denning on 3/20/20.
+//  Created by Kirsten Garthwaite on 2/25/20.
 //  Copyright © 2020 Jordan Denning. All rights reserved.
 //
 
 import Foundation
+import UIKit
+import Firebase
+
+extension UIScreen {
+    
+    enum SizeType: CGFloat {
+        case Unknown = 0.0
+        case iPhone4 = 960.0
+        case iPhone5 = 1136.0
+        case iPhone6 = 1334.0
+        case iPhone6Plus = 1920.0
+    }
+    
+    var sizeType: SizeType {
+        let height = nativeBounds.height
+        guard let sizeType = SizeType(rawValue: height) else { return .Unknown }
+        return sizeType
+    }
+}
