0800a8101e9bc961ca0fafb78bf6a96c6935a0c9
Jordan Denning <jode8036@colorado.edu>
2020-03-20 15:46:49 -0600

    put screen size helper in helpers file

---
+++ StrideLearningApp/Helpers/ScreenSizeExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ScreenSizeExtension.swift
+//  StrideLearningApp
+//
+//  Created by Jordan Denning on 3/20/20.
+//  Copyright © 2020 Jordan Denning. All rights reserved.
+//
+
+import Foundation
