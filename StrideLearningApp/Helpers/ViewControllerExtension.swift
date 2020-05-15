80eddac8e43500e150e2f664b9ed5df4b9a1ccd1
Jordan Denning <jode8036@colorado.edu>
2020-05-15 12:18:55 -0600

    If in chatViewController, don't recieve notification banner

---
+++ StrideLearningApp/Helpers/ViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerExtension.swift
+//  StrideLearningApp
+//
+//  Created by Jordan Denning on 5/15/20.
+//  Copyright © 2020 Jordan Denning. All rights reserved.
+//
+
+import Foundation
